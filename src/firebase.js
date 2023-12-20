599cae7cd3e4e7b8eef8a03d99d2eb177fc702e7
localhostd3veloper <gautamanand022@gmail.com>
2023-12-21 02:26:31 +0530

    firebase js updates

--- src/firebase.js
+++ src/firebase.js
@@ -1,24 +1,22 @@
-// import { initializeApp } from "firebase/app";
-import firebase from "firebase/compat/app";
-import { getAuth } from "firebase/auth";
-import { getAnalytics } from "firebase/analytics";
-import { getDatabase } from "firebase/database";
-import 'firebase/compat/firestore';
-import { getFirestore } from "firebase/firestore";
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+import { getAnalytics } from 'firebase/analytics';
+import { getDatabase } from 'firebase/database';
+import { getFirestore } from 'firebase/firestore/lite';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBQiu3uRke16KhV7xzfcbMc95spXCpfUv8",
-  authDomain: "energysavers-es.firebaseapp.com",
-  databaseURL: "https://energysavers-es-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "energysavers-es",
-  storageBucket: "energysavers-es.appspot.com",
-  messagingSenderId: "247785639120",
-  appId: "1:247785639120:web:903d77d43f678011b8a2d5",
-  measurementId: "G-2ESQZCQBRN"
+  apiKey: 'AIzaSyBQiu3uRke16KhV7xzfcbMc95spXCpfUv8',
+  authDomain: 'energysavers-es.firebaseapp.com',
+  databaseURL:
+    'https://energysavers-es-default-rtdb.asia-southeast1.firebasedatabase.app',
+  projectId: 'energysavers-es',
+  storageBucket: 'energysavers-es.appspot.com',
+  messagingSenderId: '247785639120',
+  appId: '1:247785639120:web:903d77d43f678011b8a2d5',
+  measurementId: 'G-2ESQZCQBRN',
 };
 
-
-const app = firebase.initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
 
 const auth = getAuth();
 
@@ -31,6 +29,5 @@ const db = getFirestore(app); // Initialize Firestore
 // console.log(auth.currentUser.getIdToken().then((token) => {
 //   console.log(token);
 // }));
-
 
 export { app, auth, analytics, database, db };
